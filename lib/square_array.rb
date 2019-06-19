9750b58d8bb930d928eca59f27afe38f8ff03e48
Jamal Farah <moulayjam@gmail.com>
2019-06-19 15:48:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,13 +6,3 @@ def square_array(numbers)
   return new_numbers
 end
 
-
-def square_array(numbers)
-    new_numbers = []
-	for x in numbers do
-    new_numbers << (x ** 2)
-  end
-
-   new_numbers
-
-end
